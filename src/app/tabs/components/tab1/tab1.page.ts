9fefa4eaba924f0e0a9df50fcbcb2b7e5da0ca02
borowybas <bar.borowy@gmail.com>
2025-01-02 17:25:55 +0100

    Created grid for words displaying and selecting

--- src/app/tabs/components/tab1/tab1.page.ts
+++ src/app/tabs/components/tab1/tab1.page.ts
@@ -3,16 +3,16 @@ import {
   IonHeader,
   IonToolbar,
   IonTitle,
-  IonContent,
-} from '@ionic/angular/standalone';
+  IonContent, IonRow, IonGrid, IonCol } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Tile } from 'src/app/models/tile.model';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [
+  imports: [IonCol, IonGrid, IonRow, 
     IonHeader,
     IonToolbar,
     IonTitle,
@@ -22,4 +22,11 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
 })
 export class Tab1Page {
   constructor() {}
+
+  wordTiles: Tile [] = [
+    {id: '1', label: 'Mama', isCustom: false, isSelected: false},
+    {id: '2', label: 'Kot', isCustom: false, isSelected: false},
+    {id: '3', label: 'Jedzenie', isCustom: false, isSelected: false},
+  ];
+
 }
