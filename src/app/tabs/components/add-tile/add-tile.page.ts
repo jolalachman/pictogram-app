5f4d08fe9db0f8b4231f49134a4307d4c64a1524
borowybas <bar.borowy@gmail.com>
2025-01-03 18:59:38 +0100

    Form for adding tiles gathers tile name

--- src/app/tabs/components/add-tile/add-tile.page.ts
+++ src/app/tabs/components/add-tile/add-tile.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonInput, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-add-tile',
   templateUrl: './add-tile.page.html',
   styleUrls: ['./add-tile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonInput, IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class AddTilePage implements OnInit {
+
+  public formData: FormGroup = new FormGroup({});
+  // private formData: FormGroup | undefined;
 
   constructor() { }
 
   ngOnInit() {
+    this.formData = new FormGroup({
+      title: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    console.log('Tile added:', this.formData.value);
   }
 
 }
